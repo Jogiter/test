471928e838b3c997f99a5b3ea4bbbc68b7963593
Jogiter <jogiter.g@gmail.com>
2018-01-22 18:24:47 +0800

    timeout

--- music/index.js
+++ music/index.js
@@ -11,14 +11,16 @@ function playSound(e) {
 	key.classList.add('playing');
 	audio.currentTime = 0;
 	audio.play();
-
-	rythm.setMusic('http://m10.music.126.net/20180122184736/8daf965222132307f3d744ec782feef8/ymusic/ef7a/b4ba/8d00/8561593800d5958a54746dce9e04a252.mp3');
-  rythm.addRythm('rythm-bass', 'jump', 0, 10)
-  rythm.start();
 }
 
-let rythm = new Rythm();
-let source;
+setTimeout(function() {
+	let rythm = new Rythm();
+	let source;
+	rythm.setMusic('http://m10.music.126.net/20180122184736/8daf965222132307f3d744ec782feef8/ymusic/ef7a/b4ba/8d00/8561593800d5958a54746dce9e04a252.mp3');
+	rythm.addRythm('rythm-bass', 'jump', 0, 10)
+	rythm.start();
+}, 500);
+
 const keys = Array.from(document.querySelectorAll('.key'));
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 window.addEventListener('keydown', playSound);
