4e2dd9f1fbf38364746f11078fa3fd1e645624bd
Jogiter <jogiter.g@gmail.com>
2018-01-22 18:19:54 +0800

    add music

---
+++ music/index.js
@@ -0,0 +1,24 @@
+function removeTransition(e) {
+	if (e.propertyName !== 'transform') return;
+	e.target.classList.remove('playing');
+}
+
+function playSound(e) {
+	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+	const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+	if (!audio) return;
+
+	key.classList.add('playing');
+	audio.currentTime = 0;
+	audio.play();
+	console.log('./sounds/boom.wav')
+	rythm.setMusic('./sounds/boom.wav');
+  rythm.addRythm('rythm-bass', 'jump', 0, 10)
+  rythm.start();
+}
+
+let rythm = new Rythm();
+let source;
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
